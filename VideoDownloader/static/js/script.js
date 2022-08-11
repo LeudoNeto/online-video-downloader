ea8d5d2fc106f8c921bbd0424065c57d23ba453f
Leudo Neto <leudoneto1234@gmail.com>
2022-08-11 17:08:43 -0300

    django

--- VideoDownloader/static/js/script.js
+++ VideoDownloader/static/js/script.js
@@ -5,29 +5,29 @@ url_input = document.querySelector('input[type=text]');
 yt = websites[0];
 yt.addEventListener('click', function(){
     url_input.placeholder = 'URL: https://www.youtube.com/watch? ...'
-    chosen_website.src = 'img/ytcomplete.png'
+    chosen_website.src = 'static/img/ytcomplete.png'
 })
 
 ig = websites[1];
 ig.addEventListener('click', function(){
     url_input.placeholder = 'URL: https://www.instagram.com/watch? ...'
-    chosen_website.src = 'img/igcomplete.png'
+    chosen_website.src = "static/img/igcomplete.png"
 })
 
 tw = websites[2];
 tw.addEventListener('click', function(){
     url_input.placeholder = 'URL: https://www.twitter.com/watch? ...'
-    chosen_website.src = 'img/twcomplete.png'
+    chosen_website.src = 'static/img/twcomplete.png'
 })
 
 pin = websites[3];
 pin.addEventListener('click', function(){
     url_input.placeholder = 'URL: https://www.pinterest.com/watch? ...'
-    chosen_website.src = 'img/pincomplete.png'
+    chosen_website.src = 'static/img/pincomplete.png'
 })
 
 rd = websites[4];
 rd.addEventListener('click', function(){
     url_input.placeholder = 'URL: https://www.reddit.com/watch? ...'
-    chosen_website.src = 'img/rdcomplete.png'
+    chosen_website.src = 'static/img/rdcomplete.png'
 })
