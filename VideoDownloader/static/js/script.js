53556155c1fc15f4a7d8890ae1ef1c435594aa05
Leudo Neto <leudoneto1234@gmail.com>
2022-08-12 11:53:08 -0300

    yt v1

--- VideoDownloader/static/js/script.js
+++ VideoDownloader/static/js/script.js
@@ -1,33 +1,36 @@
 websites = document.querySelectorAll('label');
 chosen_website = document.getElementById('chosen_website');
 url_input = document.querySelector('input[type=text]');
+download_button = document.getElementById('yt')
 
 yt = websites[0];
 yt.addEventListener('click', function(){
-    url_input.placeholder = 'URL: https://www.youtube.com/watch? ...'
-    chosen_website.src = 'static/img/ytcomplete.png'
+    url_input.placeholder = 'URL: https://www.youtube.com/watch? ...';
+    chosen_website.src = 'static/img/ytcomplete.png';
+    download_button.id = 'yt';
 })
 
 ig = websites[1];
 ig.addEventListener('click', function(){
-    url_input.placeholder = 'URL: https://www.instagram.com/watch? ...'
-    chosen_website.src = "static/img/igcomplete.png"
+    url_input.placeholder = 'URL: https://www.instagram.com/watch? ...';
+    chosen_website.src = "static/img/igcomplete.png";
+    download_button.id = 'ig';
 })
 
 tw = websites[2];
 tw.addEventListener('click', function(){
-    url_input.placeholder = 'URL: https://www.twitter.com/watch? ...'
-    chosen_website.src = 'static/img/twcomplete.png'
+    url_input.placeholder = 'URL: https://www.twitter.com/watch? ...';
+    chosen_website.src = 'static/img/twcomplete.png';
 })
 
 pin = websites[3];
 pin.addEventListener('click', function(){
-    url_input.placeholder = 'URL: https://www.pinterest.com/watch? ...'
-    chosen_website.src = 'static/img/pincomplete.png'
+    url_input.placeholder = 'URL: https://www.pinterest.com/watch? ...';
+    chosen_website.src = 'static/img/pincomplete.png';
 })
 
 rd = websites[4];
 rd.addEventListener('click', function(){
-    url_input.placeholder = 'URL: https://www.reddit.com/watch? ...'
-    chosen_website.src = 'static/img/rdcomplete.png'
+    url_input.placeholder = 'URL: https://www.reddit.com/watch? ...';
+    chosen_website.src = 'static/img/rdcomplete.png';
 })
